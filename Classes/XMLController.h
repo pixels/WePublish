2f0ee5e96d185b91b2fecc53310b58648801c49f
Yusuke Kikkawa <kikkawa@yuimaar.com>
2010-08-05 09:42:24 +0900

    First commit.

---
+++ Classes/XMLController.h
@@ -0,0 +1,31 @@
+//
+//  XMLController.h
+//  WePublish
+//
+//  Created by Yusuke Kikkawa on 10/07/13.
+//  Copyright 2010 3di. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BookInfo;
+@class BookCollection;
+
+@interface XMLController : NSObject {
+	NSUInteger _updateRetryCount;
+	NSString* _url;
+	NSString* _userFilePath;
+	NSString* _outputFilePath;
+	NSMutableData* _data;
+	NSString *_currentNodeType;
+	BookInfo *_currentBookInfo;
+	BookCollection *_bookCollection;
+}
+
+- (void)update:(NSString*)url;
+- (void)updateContinue;
+- (void)saveXML;
+- (void)alertIfDontExistData:(NSString *)message;
+- (void)parse:(NSData *)data;
+
+@end
